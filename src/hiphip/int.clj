32d87406fd106a78bdba8be37b4b1d902cddf7c5
Jason Wolfe <jason@w01fe.com>
2013-10-06 12:36:03 -0700

    Switch from load to load-string, seems to fix AOT issues

--- src/hiphip/int.clj
+++ src/hiphip/int.clj
@@ -1,8 +1,9 @@
 (ns hiphip.int
   "Utilities for int arrays"
   (:refer-clojure :exclude [amap areduce alength aget aset aclone])
+  (:require [hiphip.impl.core :as impl])
   (:import hiphip.int_.Helpers))
 
 (def +type+ 'int)
 
-(load "type_impl")
+(load-string (impl/slurp-from-classpath "hiphip/type_impl.clj"))
