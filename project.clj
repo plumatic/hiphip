33143512e5c901d4c9e65c092726177a944a6981
Jason Wolfe <jason@w01fe.com>
2015-04-08 11:55:53 -0700

    Test on latest Clojure, and default to 1.6

--- project.clj
+++ project.clj
@@ -7,11 +7,12 @@
   :url "https://github.com/Prismatic/hiphip"
   :java-source-paths ["java"]
   :profiles {:dev {:plugins [[lein-marginalia "0.7.1"]]
-                   :dependencies [[org.clojure/clojure "1.5.1"]
+                   :dependencies [[org.clojure/clojure "1.6.0"]
                                   [criterium "0.4.1"]
                                   [org.clojure/test.generative "0.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6"]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha6"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7"]}
   :test-selectors {:fast #(not (or (:bench %) (:gen-test %)))
                    :gen-test :gen-test
                    :bench :bench}
